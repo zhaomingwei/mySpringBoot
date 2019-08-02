11a942b957bbf2ee95b5a866c0a02ac5f6acf007
zhaowei <874336775@qq.com>
2019-08-02 17:59:47 +0800

    f

--- src/main/resources/sql/allSql.sql
+++ src/main/resources/sql/allSql.sql
@@ -1,6 +1,7 @@
 create table user(
 	id varchar(36) not null comment '用户id',
 	username varchar(45) null comment '用户名',
-  passwd varchar(36) null comment '用户密码',
-	age int(3) not null default 0 comment '用户年龄'
+    passwd varchar(36) null comment '用户密码',
+	age int(3) not null default 0 comment '用户年龄',
+	primary key(id)
 )
