790210fcfa4b1450f2d0f10ef126e9682df57def
zhaowei <874336775@qq.com>
2019-08-08 17:45:27 +0800

    if else 改为策略模式案例-完善，测试方法报错待解决

--- src/main/java/com/zw/cn/myspringboot/demo/ifelse2strage/InspectionTest.java
+++ src/main/java/com/zw/cn/myspringboot/demo/ifelse2strage/InspectionTest.java
@@ -1,6 +1,6 @@
 package com.zw.cn.myspringboot.demo.ifelse2strage;
 
-import javafx.application.Application;
+import com.sun.glass.ui.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
