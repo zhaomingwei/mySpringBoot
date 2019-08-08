63cbcb10d1c91056b9106938aff39930f80cf39a
zhaowei <874336775@qq.com>
2019-08-08 17:09:52 +0800

    if else 改为策略模式案例

---
+++ src/main/java/com/zw/cn/myspringboot/demo/strage/InspectionSolverChooser.java
@@ -0,0 +1,45 @@
+package com.zw.cn.myspringboot.demo.strage;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhaowei
+ * Date: 2019/8/8
+ * Time: 17:02
+ * To change this template use File | Settings | File Templates.
+ * Description: TODO
+ */
+@Component
+public class InspectionSolverChooser implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    private Map<String, InspectionSolver> chooseMap = new HashMap<>();
+
+    public InspectionSolver choose(String type){
+        return chooseMap.get(type);
+    }
+
+    @PostConstruct
+    public void register(){
+        Map<String, InspectionSolver> solverMap = applicationContext.getBeansOfType(InspectionSolver.class);
+        for(InspectionSolver solver : solverMap.values()){
+            for(String support : solver.supports()){
+                chooseMap.put(support, solver);
+            }
+        }
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
